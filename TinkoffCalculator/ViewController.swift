172cb439d254eabe812ec9c816c7ec5317a9e3bf
enneract9 <ottoblau@yandex-team.ru>
2024-01-25 22:12:18 +0500

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by @_@ on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
